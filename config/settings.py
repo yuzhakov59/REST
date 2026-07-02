058e333c0c841cb782594bba459e828a4c8095c8
yuzhakov <yuzhakov.a.a@mail.ru>
2026-07-02 12:13:50 +0500

    REST 33 Celery

--- config/settings.py
+++ config/settings.py
@@ -91,7 +91,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Yekaterinburg'
 
 USE_I18N = True
 
@@ -138,8 +138,21 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
 CELERY_BEAT_SCHEDULE = {
-    'task-name': {
-        'task': 'materials.tasks.my_task',  # Путь к задаче
-        'schedule': timedelta(minutes=10),  # Расписание выполнения задачи (например, каждые 10 минут)
+    'block-inactive-users-daily': {
+        # Путь к задаче в tasks.py
+        'task': 'migrations.tasks.block_inactive_users',
+        'schedule': timedelta(days=1),
     },
+
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "yuzhakov.a.a@yandex.ru"
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
